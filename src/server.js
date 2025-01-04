b2be16d9951c78da1bd699df7e39f9c0c3c6cb57
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2025-01-04 12:01:39 +0100

    refactor server.js to load environment variables based on NODE_ENV and improve logging

--- src/server.js
+++ src/server.js
@@ -1,21 +1,29 @@
-// src/server.js
-require('dotenv').config();
+const dotenv = require('dotenv');
+
+// Load environment variables based on NODE_ENV
+if (process.env.NODE_ENV === 'test') {
+  dotenv.config({ path: '.env.test' });
+} else {
+  dotenv.config();
+}
+
 const app = require('./app');
+const logger = require('./utils/logger');
 
 const PORT = process.env.PORT || 3001;
 
 init();
 
 async function init() {
-    try {
-        await app.get('sequelize').authenticate();
-        console.log('Database connected successfully.');
+  try {
+    await app.get('sequelize').authenticate();
+    logger.info('Database connected successfully.');
 
-        app.listen(PORT, () => {
-            console.log(`Express App Listening on Port ${PORT}`);
-        });
-    } catch (error) {
-        console.error(`An error occurred: ${error.message}`);
-        process.exit(1);
-    }
+    app.listen(PORT, () => {
+      logger.info(`Express App Listening on Port ${PORT}`);
+    });
+  } catch (error) {
+    logger.error(`An error occurred: ${error.message}`);
+    process.exit(1);
+  }
 }
