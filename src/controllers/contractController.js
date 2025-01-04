c7a12f4d8f59b3360dccbe3d8a209d3f6c80f235
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2025-01-04 15:53:16 +0100

    refactor contractController to improve query handling and error management; enhance readability with consistent formatting

--- src/controllers/contractController.js
+++ src/controllers/contractController.js
@@ -1,34 +1,53 @@
-const { Contract } = require('../models/model');
+const { Contract } = require("../models/model");
+const { Op } = require("sequelize");
 
 const getContractById = async (req, res) => {
-    const { Contract } = req.app.get('models');
-    const { id } = req.params;
-    const profileId = req.profile.id;
-
-    const contract = await Contract.findOne({
-        where: { id, [Contract.sequelize.Op.or]: [{ ClientId: profileId }, { ContractorId: profileId }] },
-    });
-
-    if (!contract) return res.status(404).json({ error: 'Contract not found or access denied' });
-    res.json(contract);
-};
-
-const getContracts = async (req, res) => {
-  const { Contract } = req.app.get('models');
+  const { Contract } = req.app.get("models");
+  const { id } = req.params;
   const profileId = req.profile.id;
-  const { page = 1, limit = 10 } = req.query;
 
-  const offset = (page - 1) * limit;
-  const contracts = await Contract.findAll({
-      where: {
-          [Contract.sequelize.Op.or]: [{ ClientId: profileId }, { ContractorId: profileId }],
-          status: { [Contract.sequelize.Op.ne]: 'terminated' },
-      },
-      limit: Number.parseInt(limit),
-      offset: Number.parseInt(offset),
+  const contract = await Contract.findOne({
+    where: {
+      id,
+      [Op.or]: [
+        { ClientId: profileId },
+        { ContractorId: profileId },
+      ],
+    },
   });
 
-  res.json(contracts);
+  if (!contract)
+    return res
+      .status(404)
+      .json({ error: "Contract not found or access denied" });
+  res.json(contract);
+};
+
+const getContracts = async (req, res, next) => {
+  const profileId = req.profile.id;
+  const { status } = req.query;
+  const limit = req.query.limit || 10; // Default limit
+
+  try {
+    const whereClause = {
+      [Op.or]: [{ ClientId: profileId }, { ContractorId: profileId }],
+    };
+
+    if (status === "terminated") {
+      whereClause.status = "terminated";
+    } else {
+      whereClause.status = { [Op.ne]: "terminated" };
+    }
+
+    const contracts = await Contract.findAll({
+      where: whereClause,
+      limit: Number.parseInt(limit),
+    });
+
+    res.json(contracts);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = { getContractById, getContracts };
